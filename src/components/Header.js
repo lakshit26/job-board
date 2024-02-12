8795e97d96ffdb85dbb366abdcee7da19b702e11
Lakshit <lakshit.vyas.eee20@itbhu.ac.in>
2024-02-12 21:43:51 +0530

    added navbar along with css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,38 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Link } from 'react-router-dom';
-import './styles/header.css'
+import '../styles/reset.min.css';
+import '../styles/header.css'
+
 function Header() {
+  const [isNavActive, setIsNavActive] = useState(false);
+  function handleNavToggle(){
+    setIsNavActive(!isNavActive);
+  }
   return (
-    <nav>
-      <ul>
-        <Link to='/'>Home</Link>
-        <Link to='/about-us'>About Us</Link>
-        <Link to='/find-jobs'>Find Jobs</Link>
-        <Link to='/upload-job'>Upload Jobs</Link>
-        <Link to='/companies'>Companies</Link>
-      </ul>
-    </nav>
+    <header class="site-header">
+      <div class="wrapper site-header__wrapper">
+        <div class="site-header__start">
+        <Link to='/' class="brand">Brand</Link>
+        </div>
+        <div class="site-header__middle">
+          <nav class="nav">
+            <button onClick={handleNavToggle} class="nav__toggle" aria-expanded="false" type="button">
+              menu
+            </button>
+            <ul class={"nav__wrapper"+(isNavActive?" active":"")}>
+              <Link to='/' class="nav__item">Home</Link>
+              <Link to='/about-us' class="nav__item">About Us</Link>
+              <Link to='/find-jobs' class="nav__item">Find Jobs</Link>
+              <Link to='/upload-job' class="nav__item">Upload Job</Link>
+              <Link to='/companies' class="nav__item">Companies</Link>
+            </ul>
+          </nav>
+        </div>
+        <div class="site-header__end">
+          <a>Sign in</a>
+        </div>
+      </div>
+    </header>
   );
 }
 
